b5f389ff36a3c5f84727c6ea88db5f197d37d901
Otavio Napoli <otavio.napoli@gmail.com>
2022-02-10 22:33:04 +0000

    House keeping

--- old_files/librep/actions/action.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-from tqdm.contrib.concurrent import thread_map
-
-class Action(ABC):   
-    @abstractmethod
-    def run(self, input_data: Any, metadata: dict):
-        raise NotImplementedError("Must be implemented in dervived classes")
